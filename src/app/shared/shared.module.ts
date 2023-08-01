d1a99c7b6900a82229e72b37cd2fc2c6f11aa763
Saied <elsaied.samaka1997@gmail.com>
2023-08-01 23:05:05 +0300

    add popup component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ModalComponent } from './modal/modal.component';
+import { PopupComponent } from './popup/popup.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [ModalComponent],
-  exports: [ModalComponent],
+  declarations: [ModalComponent, PopupComponent],
+  exports: [ModalComponent, PopupComponent],
 })
 export class SharedModule {}
