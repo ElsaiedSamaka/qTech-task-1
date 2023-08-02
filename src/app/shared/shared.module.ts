186ed3c47b45a53231a4209c488c91d0c67e9aa3
Saied <elsaied.samaka1997@gmail.com>
2023-08-03 00:21:08 +0300

    enhance

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { InputComponent } from './input/input.component';
 import { ModalComponent } from './modal/modal.component';
 import { PopupComponent } from './popup/popup.component';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [ModalComponent, PopupComponent],
-  exports: [ModalComponent, PopupComponent],
+  imports: [CommonModule, ReactiveFormsModule],
+  declarations: [ModalComponent, PopupComponent, InputComponent],
+  exports: [ModalComponent, PopupComponent, InputComponent],
 })
 export class SharedModule {}
