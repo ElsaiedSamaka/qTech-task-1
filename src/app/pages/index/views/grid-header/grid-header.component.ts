a17988a441dc1202802173101a2714e0093319c8
Saied <elsaied.samaka1997@gmail.com>
2023-08-02 16:54:35 +0300

    enhance

--- src/app/pages/index/views/grid-header/grid-header.component.ts
+++ src/app/pages/index/views/grid-header/grid-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-grid-header',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   standalone: true,
 })
 export class GridHeaderComponent implements OnInit {
+  @Input() data: any[] = [];
   constructor() {}
 
   ngOnInit() {}
