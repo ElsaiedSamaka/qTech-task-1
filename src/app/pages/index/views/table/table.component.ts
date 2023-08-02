0feb441c38ed9d62cb920ebea36ae440d7053d21
Saied <elsaied.samaka1997@gmail.com>
2023-08-02 16:31:27 +0300

    enhance

--- src/app/pages/index/views/table/table.component.ts
+++ src/app/pages/index/views/table/table.component.ts
@@ -1,4 +1,4 @@
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
@@ -6,7 +6,7 @@ import { Component, Input, OnInit } from '@angular/core';
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css'],
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, NgIf],
 })
 export class TableComponent implements OnInit {
   @Input() data: any[] = [];
