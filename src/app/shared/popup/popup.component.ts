4a81cd3013be6530258f0f7be9d3b7de2a6346ff
Saied <elsaied.samaka1997@gmail.com>
2023-08-04 17:28:35 +0300

    enhance

--- src/app/shared/popup/popup.component.ts
+++ src/app/shared/popup/popup.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  Output,
+} from '@angular/core';
 
 @Component({
   selector: 'app-popup',
   templateUrl: './popup.component.html',
-  styleUrls: ['./popup.component.css']
+  styleUrls: ['./popup.component.css'],
 })
 export class PopupComponent implements OnInit {
+  @Output() dismiss = new EventEmitter<boolean>();
 
-  constructor() { }
+  constructor(private el: ElementRef) {}
 
   ngOnInit() {
+    document.body.appendChild(this.el.nativeElement);
   }
-
+  ngOnDestory() {
+    this.el.nativeElement.remove();
+  }
+  onDismissClick() {
+    this.dismiss.emit(false);
+  }
 }
