706c8f3c97a1b81d14a5c98df07315f720f68691
Saied <elsaied.samaka1997@gmail.com>
2023-08-05 16:33:49 +0300

    enhance

--- src/app/pages/index/views/grid/grid.component.ts
+++ src/app/pages/index/views/grid/grid.component.ts
@@ -118,27 +118,18 @@ export class GridComponent implements OnInit {
       !this.showDeletetionConfirmationModal;
   }
   public onGoTo(page: number): void {
-    // this.page = page;
-    // this.usersToDisplay = this.paginate(this.page, this.size);
+    this.page = page;
+    this.getUsers(this.page, this.size);
   }
 
   public onNext(page: number): void {
-    // if (this.page === this.totalPages) return;
-    // if (this.page < this.totalPages) {
-    //   this.page++;
-    //   this.usersToDisplay = this.paginate(this.page, this.size);
-    // }
+    this.page = page;
+    this.getUsers(this.page, this.size);
   }
 
   public onPrevious(page: number): void {
-    // if (this.page === 1) return;
-    // if (this.page > 1) {
-    //   this.page--;
-    //   this.usersToDisplay = this.paginate(this.page, this.size);
-    // }
+    this.page = page;
+    this.getUsers(this.page, this.size);
   }
 
-  public paginate(current: number, size: number): any[] {
-    return [...this.users.slice((current - 1) * size).slice(0, size)];
-  }
 }
